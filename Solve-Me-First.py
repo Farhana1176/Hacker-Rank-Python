67730908ecc6e1648166c2afc669b05fa8360ff9
Farhana Yasmin <69351474+Farhana1176@users.noreply.github.com>
2022-08-23 09:00:43 +0530

    Create Solve-Me-First.py

---
+++ Solve-Me-First.py
@@ -0,0 +1,23 @@
+Complete the function solveMeFirst to compute the sum of two integers.
+Example:
+a = 7
+b = 3
+Return 10.
+
+Function Description:
+Complete the solveMeFirst function in the editor below.
+
+SolveMeFirst has the following parameters:
+int a: the first value
+int b: the second value
+Returns
+- int: the sum of a and b. 
+
+---------CODE---------------------
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a + b
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
